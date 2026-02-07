3fe6465c2e9f80518bba685078770dffdca45ca2
Abdullah Khamis <abdullahbkhamis@gmail.com>
2026-02-06 20:31:08 -0500

    enabled cors on backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,23 @@
 import { Elysia } from "elysia"
 import { createWalletRoutes } from "./modules/wallet";
+import cors from "@elysiajs/cors";
+import { CLIENT_URL } from "./modules/constants/env.constants";
 
 const PORT = process.env.PORT || 3000;
 
-new Elysia().get("/api/ping", () => {
-  return {
-    pong: true,
-    timestamp: new Date().toISOString(),
-    status: "ok"
-  };
-}).group("/api", (group) => group.use(createWalletRoutes())).listen(PORT);
+new Elysia()
+  .use(cors({
+    origin: [CLIENT_URL],
+    methods: ["POST", "GET", "PATCH", "DELETE"]
+  }))
+  .get("/api/ping", () => {
+    return {
+      pong: true,
+      timestamp: new Date().toISOString(),
+      status: "ok"
+    };
+  })
+  .group("/api", (group) => group.use(createWalletRoutes())).listen(PORT);
 
 console.log(
   `🚀 Ethereum Wallet Market Analysis server is running at http://localhost:${PORT}`
